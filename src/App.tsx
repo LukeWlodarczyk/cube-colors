6beeb5e6f55c06b1dab8aacf758bd73a66d750e5
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2026-05-14 04:22:27 +0200

    feat: add swipe gestures

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useSwipeable } from "react-swipeable";
 import Heading from "./components/Heading";
 import ColorTile from "./components/ColorTile";
 import Options from "./components/Options";
@@ -32,11 +33,20 @@ const App = () => {
     KeyT: () => mode.set("triple"),
   });
 
+  const swipeable = useSwipeable({
+    onSwipedLeft: colors.current.prev,
+    onSwipedRight: colors.current.next,
+    preventScrollOnSwipe: true,
+  });
+
   return (
     <div className="min-h-screen bg-dark text-white flex flex-col">
       <Heading className="mt-10 mb-24">Cube Color Schema</Heading>
 
-      <div className="flex justify-center items-center gap-8 md:gap-16 mx-4 md:mx-18">
+      <div
+        {...swipeable}
+        className="flex justify-center items-center gap-8 md:gap-16 mx-4 md:mx-18"
+      >
         <ColorTile
           size="large"
           color={colors.current.neighbors.left}
