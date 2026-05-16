e12f3fa98348cf5829f955e3120f5ff18c6b5119
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2026-05-16 15:46:01 +0200

    feat: add basic pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import { registerSW } from "virtual:pwa-register";
+
+import "./index.css";
+
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+registerSW({
+  immediate: true,
+});
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,47 @@ import babel from "@rolldown/plugin-babel";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import { VitePWA } from "vite-plugin-pwa";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "Cube Colors",
+        short_name: "Cube",
+        description: "An app for practicing Rubik’s Cube color recognition",
+        start_url: "/",
+        scope: "/",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/web-app-manifest-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
   ],
 });
